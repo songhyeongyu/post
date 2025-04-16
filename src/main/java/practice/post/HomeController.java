752663548ef6c9824eb0d2b3ac6c90296e2b92eb
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-16 23:42:29 +0900

    fix : add session method

--- src/main/java/practice/post/HomeController.java
+++ src/main/java/practice/post/HomeController.java
@@ -1,13 +1,32 @@
 package practice.post;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import practice.post.domain.Member;
+import practice.post.web.SessionConst;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String home(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            return "home";
+        }
+
+        Member login = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if (login == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", login);
+
+        return "loginHome";
     }
 
 }
