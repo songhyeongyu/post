4bf0a807a90e9fc352b4e5973e2ec39ede8e65d5
song hyeongyu <lit3207@ajou.ac.kr>
2025-04-16 23:35:37 +0900

    fix : refactor MemberController Validate

--- src/main/java/practice/post/controller/MemberController.java
+++ src/main/java/practice/post/controller/MemberController.java
@@ -3,7 +3,10 @@ package practice.post.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,7 @@ import practice.post.domain.Member;
 import practice.post.dto.MemberJoinDto;
 import practice.post.repository.MemberRepository;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/members")
@@ -27,8 +31,24 @@ public class MemberController {
     /**
      * String username,String nickname, String encryptedPassword, String email, String phoneNumber
      */
-    @PostMapping("/add")
-    public String join(@Valid @ModelAttribute("memberJoinDto") MemberJoinDto dto) {
+    @PostMapping("/add") @Transactional
+    public String join(@Valid @ModelAttribute("memberJoinDto") MemberJoinDto dto, BindingResult br) {
+
+
+        if (memberRepository.findByNickName(dto.getNickname()).isPresent()) {
+            br.rejectValue("nickname", "duplicate.nickname", "이미 존재하는 아이디입니다.");
+        }
+        if (memberRepository.findByEmail(dto.getEmail()).isPresent()) {
+            br.rejectValue("email", "duplicate.email", "이미 존재하는 이메일입니다.");
+        }
+        if (memberRepository.findByPhoneNumber(dto.getPhoneNumber()).isPresent()) {
+            br.rejectValue("phoneNumber", "duplicate.phoneNumber", "이미 존재하는 핸드폰 번호입니다.");
+        }
+        if (br.hasErrors()) {
+            log.info("errors={}",br);
+            return "members/addMemberForm";
+        }
+
         Member member = Member.create(
                 dto.getUsername(),
                 dto.getNickname(),
@@ -39,5 +59,7 @@ public class MemberController {
         memberRepository.save(member);
         return "redirect:/";
     }
+
+
 
 }
